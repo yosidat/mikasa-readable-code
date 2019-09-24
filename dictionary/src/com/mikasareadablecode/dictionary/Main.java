4e20463aafa304f70697abd32ec7161dc96efbbb
Hiroki Mikasa <hiroki.mikasa@justsystems.com>
2019-09-24 13:49:47 +0900

    仕様3: 出力用のファイル（src/resource/message.txt）の切り分け。

--- dictionary/src/com/mikasareadablecode/dictionary/Main.java
+++ dictionary/src/com/mikasareadablecode/dictionary/Main.java
@@ -1,12 +1,56 @@
 package com.mikasareadablecode.dictionary;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-       outputResult();
+        String filepath = getFilepathFromArguments(args);
+        String message = readMessageResource(filepath);
+        outputResult(message);
     }
 
-    public static void outputResult(){
-        System.out.println("上手");
+    /**
+     * プログラム実行時の引数からユーザーが指定した値を取得（第1引数）
+     * @param args プログラム実行時引数
+     * return ファイルパス(第一引数)
+     */
+    public static String getFilepathFromArguments(String[] args){
+        return args[0];
+    }
+
+    /**
+     * メッセージリソースの取得
+     * @param filepath ファイルパスの記述
+     */
+    public static String readMessageResource(String filepath) {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader= null;
+        String resultText = null;
+        try {
+            fileReader = new FileReader(filepath);
+            bufferedReader = new BufferedReader(fileReader);
+
+            resultText = resultText = bufferedReader.readLine();
+        }catch(FileNotFoundException e){
+            //TODO: エラーハンドリング
+        }catch(IOException e){
+            //TODO: エラーハンドリング
+        }finally {
+            //TODO: ファイルクローズ
+            //fileReader.close();
+            //bufferedReader.close();
+        }
+        return resultText;
+    }
+
+    /**
+     * 結果出力処理
+     */
+    public static void outputResult(String text){
+        System.out.println(text);
     }
 }
