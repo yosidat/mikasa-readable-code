b78ecb6a9c6feca62e4f2812e32aff188edf1181
Hiroki Mikasa <hiroki.mikasa@justsystems.com>
2019-09-24 11:37:15 +0900

    仕様1：結果表示の実装

---
+++ dictionary/src/com/mikasareadablecode/dictionary/Main.java
@@ -0,0 +1,12 @@
+package com.mikasareadablecode.dictionary;
+
+public class Main {
+
+    public static void main(String[] args) {
+       outputResult();
+    }
+
+    public static void outputResult(){
+        System.out.println("上手");
+    }
+}
